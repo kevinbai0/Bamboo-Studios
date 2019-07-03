0b1bae805ebd4bf1fecfd11c8e654aea9d8c46ae
Kevin Bai <kevin.c.bai0@gmail.com>
2019-07-03 17:33:00 -0400

    New commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ const ButtonsContainer = styled.div`
 `;
 
 const ContactContainer = styled.div`
-  
+
 `;
 
 const IndexPage = () => (
@@ -81,7 +81,7 @@ const IndexPage = () => (
       </ButtonsContainer>
     </SecondaryContainer>
     <ContactContainer>
-
+      
     </ContactContainer>
   </Layout>
 )
