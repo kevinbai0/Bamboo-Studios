889830329053bea0cf23de79e3720477f57c8253
kevinbai0 <kevin.c.bai0@gmail.com>
2019-07-06 00:15:55 -0400

    modfied for heroku

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ export default function Footer() {
         }
     `);
 
-    const [ usingMobileFooter, setUsingMobileFooter ] = useState(true);
+    const [ usingMobileFooter, setUsingMobileFooter ] = useState(window.innerWidth < mobileWidth);
 
     const resizeListener = () => {
         if (window.innerWidth >= mobileWidth && usingMobileFooter) setUsingMobileFooter(false);
